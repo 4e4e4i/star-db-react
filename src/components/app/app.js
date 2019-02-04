49d878c0e6a12564122509080a1893cb48cdc1df
Vasiliy Zozulya <zazevang@gmail.com>
2019-02-05 02:21:32 +0300

    добавил лоадер и кнопку отключения компонента randomPlanet

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,4 @@
-import React, { Component } from 'react';
-
-import './app.css'
+import React, {Component} from 'react';
 
 // Components
 import Header from '../header';
@@ -8,19 +6,43 @@ import RandomPlanet from '../random-planet';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
 
+import './app.css'
+
 export default class App extends Component {
+
+    state = {
+        showRandomPlanet: true
+    };
+
+    toggleRandomPlanet = () => {
+        this.setState((state) => {
+            return {
+                showRandomPlanet: !state.showRandomPlanet
+            }
+        })
+    };
+
     render() {
+
+        const planet = this.state.showRandomPlanet ? <RandomPlanet/> : null;
+
         return (
             <div className="stardb-app">
-                <Header></Header>
-                <RandomPlanet></RandomPlanet>
+                <Header/>
+                { planet }
+
+                <button
+                    className="toggle-planet btn btn-warning btn-lg"
+                    onClick={this.toggleRandomPlanet}>
+                    Toggle random planet
+                </button>
 
                 <div className="row mb2">
                     <div className="col-md-6">
-                        <ItemList></ItemList>
+                        <ItemList/>
                     </div>
                     <div className="col-md-6">
-                        <PersonDetails></PersonDetails>
+                        <PersonDetails/>
                     </div>
                 </div>
             </div>
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './spinner.scss';
+
+const Spinner = () => {
+    return (
+        <div className="spinner">
+            <div className="spinner__double-ring">
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
