f7e7a61b7cd60d7d43096bd1e50368201b3b7417
Vasiliy Zozulya <zazevang@gmail.com>
2019-02-20 22:29:52 +0300

    Updates

---
+++ precache-manifest.3a3234dcea4d66c66f2fe88ac8a1cc26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0904ab033bf16ec02f7",
+    "url": "/star-db-react/static/css/main.b668f623.chunk.css"
+  },
+  {
+    "revision": "e0904ab033bf16ec02f7",
+    "url": "/star-db-react/static/js/main.e0904ab0.chunk.js"
+  },
+  {
+    "revision": "9af2b34111ba9da8898d",
+    "url": "/star-db-react/static/js/1.9af2b341.chunk.js"
+  },
+  {
+    "revision": "792989af3bb2421a7fdd",
+    "url": "/star-db-react/static/js/runtime~main.792989af.js"
+  },
+  {
+    "revision": "c6d9cee42f49e90ff0b12df6db4e0814",
+    "url": "/star-db-react/index.html"
+  }
+];
